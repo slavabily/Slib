22d90cb57b0a242533744200a22cc8e1b24e6ccf
slava bily <slavabily@me.com>
2023-01-19 14:49:18 +0200

    Initial project setup

---
+++ Slib/Slib.h
@@ -0,0 +1,18 @@
+//
+//  Slib.h
+//  Slib
+//
+//  Created by slava bily on 12.01.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Slib.
+FOUNDATION_EXPORT double SlibVersionNumber;
+
+//! Project version string for Slib.
+FOUNDATION_EXPORT const unsigned char SlibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slib/PublicHeader.h>
+
+
---
+++ SlibExamples/SlibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlibExamplesApp.swift
+//  SlibExamples
+//
+//  Created by slava bily on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
