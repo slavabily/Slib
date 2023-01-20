1fc079ee99c5975c974b723293849966324d8d15
slava bily <slavabily@me.com>
2023-01-20 15:37:17 +0200

    added test cases

---
+++ Slib/Slib.swift
@@ -0,0 +1,22 @@
+//
+//  Slib.swift
+//  Slib
+//
+//  Created by slava bily on 20.01.2023.
+//
+
+import Foundation
+
+public final class Slib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SlibTests/SlibTests.swift
+++ SlibTests/SlibTests.swift
@@ -8,29 +8,20 @@
 import XCTest
 @testable import Slib
 
-final class SlibTests: XCTestCase {
+class SwiftyLibTests: XCTestCase {
+    
+    var swiftyLib: Slib!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        swiftyLib = Slib()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSub() {
+            XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
     }
 
 }
